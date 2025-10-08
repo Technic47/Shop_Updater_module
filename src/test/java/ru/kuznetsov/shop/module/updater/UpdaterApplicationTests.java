05d532cbdd0c4e2470c7c6c478e597b6d4b21112
Pavel Kuznetsov <Technic47@gmail.com>
2025-10-08 16:53:24 +0300

    Init commit.

---
+++ src/test/java/ru/kuznetsov/shop/module/updater/UpdaterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kuznetsov.shop.module.updater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpdaterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
